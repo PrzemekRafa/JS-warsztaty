a669958c8477ec774bc5d38bb9e0297add028bac
Przemek Rafa Mac <przemyslaw.rafa@gmail.com>
2017-06-11 14:23:14 +0200

    tlo paragrafów

---
+++ Set_background/js/set_background.js
@@ -0,0 +1,9 @@
+'use strics';
+
+var paragraf1 = document.getElementsByClassName('prgrf')[0];
+var paragraf2 = document.getElementsByClassName('prgrf')[1];
+
+function zmienKolorParagrafu () {
+    paragraf1.style.backgroundColor='red';
+    paragraf2.style.backgroundColor='yellow';
+}
